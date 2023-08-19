1bfb2358d5b7faed8791c65b6f794aba2518aa39
MiniProject6342 <132653522+MiniProject6342@users.noreply.github.com>
2023-08-19 17:11:53 +0530

    Create App.js

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import DisplayTrain from './DisplayTrain';
+import SingleTrain from './SingleTrain';
+function App() {
+  return (
+    <div className="App">
+      <DisplayTrain/>
+      <SingleTrain/>
+    </div>
+  );
+}
+
+export default App;
